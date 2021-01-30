14b8f8f907530fd78e948a257dee2ba55cf4bbf9
harper25 <olesjakubb@gmail.com>
2021-01-30 21:39:05 +0100

    Add sphinx and lock package versions

--- poetry_poems/__init__.py
+++ poetry_poems/__init__.py
@@ -1,8 +1,3 @@
 # -*- coding: utf-8 -*-
 
-""" Poems: Poetry Shell Switcher """
-
-__author__ = """Jakub Oles"""
 __version__ = "0.1.0"
-__email__ = "olesjakubb+poems@gmail.com"
-__url__ = "https://github.com/harper25/poetry-poems"
